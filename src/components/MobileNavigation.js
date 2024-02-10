4a0492b282be8fe76a7d403c1f67b71770868481
Daniel <danieldev1999@gmail.com>
2024-02-10 18:40:42 -0300

    :building_construction: Work in progress MobileNavigation component

---
+++ src/components/MobileNavigation.js
@@ -0,0 +1,19 @@
+// TODO:
+
+// 1- Add MobileNavigation component, keeping the toggle state in App.js
+// 2- Add Navigation component (for desktop)
+
+import React, { useEffect, useState } from 'react';
+import classes from './MobileNavigation.module.css';
+
+const MobileNavigation = () => {
+    useEffect(() => {
+        // Implement
+    }, []);
+
+    return (
+        <h1>MobileNav</h1>
+    );
+}
+
+export default App;
