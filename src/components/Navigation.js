83986b993734d376d48f290cbe2bb92f1b048888
Daniel <danieldev1999@gmail.com>
2024-03-09 19:27:02 -0300

    :construction: Wip

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classes from './Navigation.module.css';
 import { IoIosArrowDown } from 'react-icons/io';
+import useWindowSize from '../utils/hooks/useWIndowSize';
+
+// TODO: Pass all states to store
+// TODO: Pass all constant to a constant file
 
 const Navigation = () => {
-    // TODO: Pass to store
+    const windowSize = useWindowSize();
+    const [size, setSize] = useState('lg');
     const [toggle, setToggle] = useState({
         iconClass: classes.ArrowIcon,
         show: false,
     });
+    const [items] = useState([
+        {
+            index: 0,
+            name: 'About',
+            selected: true
+        },
+        {
+            index: 1,
+            name: 'Articles',
+            selected: false
+        },
+        {
+            index: 2,
+            name: 'Projects',
+            selected: false
+        }
+    ]);
+
+    useEffect(() => {
+        const { width } = windowSize;
+        if (width <= 768) {
+            setSize('sm');
+        } else {
+            setSize('lg');
+        }
+    }, [windowSize]);
 
     const onClick = () => {
         setToggle({
@@ -16,14 +47,26 @@ const Navigation = () => {
         });
     };
 
-    const mobileNavigationItems = () => {
-        return <button id='toggle' className={classes.Toggle} onClick={onClick}>
-            Menu <IoIosArrowDown className={toggle.iconClass} />
-        </button>;
+    const navigation = () => {
+        if (['sm', 'md'].includes(size)) {
+            return <button id='toggle' className={classes.Toggle} onClick={onClick}>
+                Menu <IoIosArrowDown className={toggle.iconClass} />
+            </button>;
+        } else {
+            return <nav className={classes.Navigation}>
+                <ul className={classes.ItemList}>
+                    {items.map((item) =>
+                        <li key={item.index} className={classes.Item}>
+                            <a className={classes.Link}> {item.name} </a>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        }
     }
 
     return (
-        mobileNavigationItems()
+        navigation()
     );
 }
 
